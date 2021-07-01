271573258e43460932192a8de844ca93a44b9660
ilnas <genatula@protonmail.com>
2021-07-01 20:45:38 +0300

    Added models

--- esl_portal/esl_app/admin.py
+++ esl_portal/esl_app/admin.py
@@ -1,19 +1,10 @@
 from django.contrib import admin
 from .models import *
 
-
-class AnswerInline(admin.StackedInline):
-    extra = 4
-    model = Answer
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    inlines = [AnswerInline]
-
-
 # Register your models here.
 admin.site.register(Test)
-admin.site.register(Question, QuestionAdmin)
+admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(Completion)
 admin.site.register(UserAnswer)
+admin.site.register(TestQuestion)
--- esl_portal/esl_app/models.py
+++ esl_portal/esl_app/models.py
@@ -5,25 +5,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Answer(models.Model):
     answer_text = models.CharField(max_length=30)
-    related_question = models.ForeignKey("Question", on_delete=models.CASCADE, blank=True, null=True)
-    is_correct = models.BooleanField(default=False)
-    user_answers = models.ManyToManyField(User, through="UserAnswer")
-
-    def __str__(self):
-        return self.answer_text
+    question_id = models.ForeignKey("Question", on_delete=models.CASCADE)
 
 
 class Question(models.Model):
-    TYPES = (
-        ("Single", "Single choice question"),
-        ("Insert", "Question with insertion"),
-        ("Multiple", "Multiple choice question")
-    )
     question_text = models.CharField(max_length=500)
-    type = models.CharField(max_length=8, choices=TYPES, default="Single")
-
-    def __str__(self):
-        return self.question_text
+    correct_answer_id = models.ForeignKey(Answer, on_delete=models.CASCADE)
 
 
 class Test(models.Model):
@@ -45,28 +32,29 @@ class Test(models.Model):
     level = models.CharField(max_length=2, choices=LEVELS)
     aspect = models.CharField(max_length=2, choices=ASPECTS)
     time_limit = models.PositiveSmallIntegerField()
-    questions = models.ManyToManyField(Question)
 
-    def __str__(self):
-        return self.test_name
+
+class TestQuestion(models.Model):
+    TYPES = (
+        ("Single", "Single choice question"),
+        ("Insert", "Question with insertion")
+    )
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    test_id = models.ForeignKey(Test, on_delete=models.CASCADE)
+    type = models.CharField(max_length=6, choices=TYPES)
 
 
 class UserAnswer(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, null=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    test_question_id = models.ForeignKey(TestQuestion, on_delete=models.CASCADE)
     answer_text = models.CharField(max_length=30)
     is_correct = models.BooleanField()
 
 
 class Completion(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    test_id = models.ForeignKey(Test, on_delete=models.CASCADE)
     is_completed = models.BooleanField()
     taken_time = models.PositiveIntegerField()
     num_of_correct = models.PositiveSmallIntegerField()
     is_started = models.BooleanField()
-
-    def __str__(self):
-        return self.user.username.__str__() + "'s completion of " + self.test.__str__()
-
-
