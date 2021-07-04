94b9ff775c3a66d78fa699b2ff22e0fff56bdffa
ilnas <genatula@protonmail.com>
2021-07-04 12:03:16 +0300

    Blank & null permissions

--- esl_portal/esl_app/models.py
+++ esl_portal/esl_app/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Answer(models.Model):
     answer_text = models.CharField(max_length=30)
-    question = models.ForeignKey("Question", on_delete=models.CASCADE, blank=True)
+    related_question = models.ForeignKey("Question", on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.answer_text
@@ -13,7 +13,7 @@ class Answer(models.Model):
 
 class Question(models.Model):
     question_text = models.CharField(max_length=500)
-    correct_answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    correct_answer = models.ForeignKey(Answer, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.question_text
