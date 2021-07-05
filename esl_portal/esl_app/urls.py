5cf4783771cb2b9262a741c627cc23d72c1815b0
Ramazan2002 <ramazanui2002@gmail.com>
2021-07-05 12:05:11 +0300

    Changed urls

---
+++ esl_portal/esl_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('main/', views.main, name='main'),
+    path('login/', views.login, name='login'),
+    path('login/forgot', views.login_forgot, name='login_forgot'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/completed/', views.profile_completed, name='profile_completed'),
+    path('profile/options/', views.profile_options, name='profile_options'),
+    path('tests/', views.test_list, name='test_list'),
+    path('tests/<int:test_id>', views.test, name='test'),
+]
