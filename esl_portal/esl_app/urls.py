3d82758b96735066368234a61525dc339649ecb3
ilnas <genatula@protonmail.com>
2021-07-05 12:16:58 +0300

    Pulled urls and views

--- esl_portal/esl_app/urls.py
+++ esl_portal/esl_app/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+
+urlpatterns = [
+
+]
+from django.urls import path
+from . import views
+
 urlpatterns = [
     path('main/', views.main, name='main'),
     path('login/', views.login, name='login'),
--- esl_portal/esl_app/views.py
+++ esl_portal/esl_app/views.py
@@ -3,17 +3,22 @@ from .models import *
 from django.urls import reverse
 # Create your views here.
 
+
 def main(request):
     pass
+
 
 def login(request):
     pass
 
+
 def login_forgot(request):
     pass
 
+
 def register(request):
     pass
+
 
 def profile(request):
     pass
@@ -21,14 +26,18 @@ def profile(request):
 def profile_completed(request):
     pass
 
+
 def profile_options(request):
     pass
+
 
 def test_list(request):
     pass
 
+
 def test(request):
     pass
+
 
 def test_result(request):
     pass
