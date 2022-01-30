a5122fe66b87b678a81ae5fb33d809b096dda4f1
ilnas <genatula@protonmail.com>
2022-01-30 21:20:35 +0300

    Fix db configuration

--- esl_portal/esl_portal/settings.py
+++ esl_portal/esl_portal/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'diploma',
-        'USER': 'psql',
+        'USER': 'postgres',
         'PASSWORD': '230764ugole',
         'HOST': '127.0.0.1',
         'PORT': '5432',
