02432360e5dc86b11631809b9d154caf6ccc9e9a
Ilnas <ilnastheclown@gmail.com>
2022-03-30 15:20:02 +0300

    Fix password

--- esl_portal/esl_portal/settings.py
+++ esl_portal/esl_portal/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'diploma',
         'USER': 'postgres',
-        'PASSWORD': '230764ugole',
+        'PASSWORD': 'simple123456',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
