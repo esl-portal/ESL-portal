7c43f0d885c3c7e09925fc916be9112b5d732619
Ilnas <ilnastheclown@gmail.com>
2022-04-09 21:48:28 +0300

    Raise ValidationError in email validation & add password validation

--- esl_portal/esl_app/validators.py
+++ esl_portal/esl_app/validators.py
@@ -1,10 +1,25 @@
 from django.core.exceptions import ValidationError
-from django.core.validators import validate_email
+from django.core.validators import validate_email, RegexValidator
+from django.utils.translation import gettext_lazy as _
 
 
 def validate_email_form(email):
     try:
         validate_email(email)
-        return True
     except ValidationError:
-        return False
+        raise ValidationError(
+            message=_('Invalid email: %(value)s'),
+            code='email',
+            params={'email': email}
+        )
+
+
+def validate_password(password):
+    password_validator = RegexValidator(regex=r'^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$')
+    try:
+        password_validator(password)
+    except ValidationError:
+        raise ValidationError(
+            message=_('Invalid password format'),
+            code='password',
+        )
