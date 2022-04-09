401a091ffe1f98ccac4b9f7471e87b0437430c27
Ilnas <ilnastheclown@gmail.com>
2022-04-09 22:01:10 +0300

    Add username validator to forms

--- esl_portal/esl_app/forms.py
+++ esl_portal/esl_app/forms.py
@@ -5,18 +5,18 @@ from django.contrib.auth.models import User
 
 
 class UserLoginForm(forms.Form):
-    username = forms.CharField(label='Введите имя пользователя', max_length=150)
+    username = forms.CharField(label='Введите имя пользователя', max_length=150, validators=[validate_username])
     password = forms.CharField(label='Введите пароль', widget=forms.PasswordInput, validators=[validate_password])
 
 
 class UserForgotForm(forms.Form):
-    username = forms.CharField(label="Введите имя пользователя", max_length=150)
+    username = forms.CharField(label="Введите имя пользователя", max_length=150, validators=[validate_username])
     password = forms.CharField(label="Введите новый пароль", widget=forms.PasswordInput, validators=[validate_password])
     password_confirmation = forms.CharField(label="Введите новый пароль ещё раз", widget=forms.PasswordInput)
 
 
 class UserRegistrationForm(forms.Form):
-    username = forms.CharField(label='Имя пользователя', max_length=150)
+    username = forms.CharField(label='Имя пользователя', max_length=150, validators=[validate_username])
     first_name = forms.CharField(label='Ваше имя')
     email = forms.EmailField(label='Адрес электронной почты', widget=forms.EmailInput, validators=[validate_email_form])
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput, validators=[validate_password])
@@ -27,7 +27,7 @@ class UserRegistrationForm(forms.Form):
 
 
 class UserChangeData(forms.Form):
-    new_username = forms.CharField(label='Имя пользователя', max_length=50)
+    new_username = forms.CharField(label='Имя пользователя', max_length=50, validators=[validate_username])
     new_first_name = forms.CharField(label='Ваше имя')
     new_email = forms.EmailField(label='Адрес электронной почты', widget=forms.EmailInput, validators=[validate_email_form])
     new_password = forms.CharField(label='Новый пароль', widget=forms.PasswordInput, required=False, validators=[validate_password])
