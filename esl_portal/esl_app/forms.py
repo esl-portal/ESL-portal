f3601c3dd873fb523bde6aa8ff34d1eca4ab8c1d
Ilnas <ilnastheclown@gmail.com>
2022-04-10 12:28:49 +0300

    Add validation to change data and forgot password forms

--- esl_portal/esl_app/forms.py
+++ esl_portal/esl_app/forms.py
@@ -15,6 +15,24 @@ class UserForgotForm(forms.Form):
     password = forms.CharField(label="Введите новый пароль", widget=forms.PasswordInput, validators=[validate_password], error_messages={'required': 'Данное поле является обязательным для заполнения'})
     password_confirmation = forms.CharField(label="Введите новый пароль ещё раз", widget=forms.PasswordInput, error_messages={'required': 'Данное поле является обязательным для заполнения'})
 
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        if not validate_password(password):
+            raise ValidationError(
+                message=_('Некорректный формат пароля'),
+                code='password',
+            )
+        return password
+
+    def clean_password2(self):
+        password_confirmation = self.cleaned_data.get('password_confirmation')
+        if not (password_confirmation == self.cleaned_data.get('password')):
+            raise ValidationError(
+                message=_('Пароли не совпадают'),
+                code='password',
+            )
+        return password_confirmation
+
 
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(label='Имя пользователя', max_length=150, validators=[validate_username], error_messages={'required': 'Данное поле является обязательным для заполнения'})
@@ -80,3 +98,42 @@ class UserChangeData(forms.Form):
     new_first_name = forms.CharField(label='Ваше имя', validators=[validate_firstname])
     new_email = forms.EmailField(label='Адрес электронной почты', widget=forms.EmailInput, validators=[validate_email])
     new_password = forms.CharField(label='Новый пароль', widget=forms.PasswordInput, required=False, validators=[validate_password])
+
+    def clean_new_email(self):
+        email = self.cleaned_data.get('new_email')
+        if not validate_email(email):
+            raise ValidationError(
+                message=_('Некорректный формат email'),
+                code='email',
+                params={'email': email}
+            )
+        return email
+
+    def clean_new_first_name(self):
+        first_name = self.cleaned_data.get('new_first_name')
+        if not validate_firstname(first_name):
+            raise ValidationError(
+                message=_('Некорректный формат имени'),
+                code='firstname',
+                params={'firstname': first_name}
+            )
+        return first_name
+
+    def clean_new_username(self):
+        username = self.cleaned_data.get('new_username')
+        if not validate_username(username):
+            raise ValidationError(
+                message=_('Некорректный формат никнейма'),
+                code='username',
+                params={'username': username}
+            )
+        return username
+
+    def clean_new_password(self):
+        password = self.cleaned_data.get('new_password')
+        if not validate_password(password):
+            raise ValidationError(
+                message=_('Некорректный формат пароля'),
+                code='password',
+            )
+        return password
